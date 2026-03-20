10939b5ca2cbaccf709f1916548d6de4e8ed723a
Diego-MCV <moisescallevasquez09@gmail.com>
2026-03-19 22:53:02 -0400

    feat: add authentication middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const verificarToken = (req, res, next) => {
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            return res.status(401).json({ error: "Token requerido" });
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.usuario = decoded; // 🔥 guardamos el usuario
+
+        next(); // continuar al controller
+    } catch (error) {
+        return res.status(401).json({ error: "Token inválido" });
+    }
+};
+
+module.exports = verificarToken;
